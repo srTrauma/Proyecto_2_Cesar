df34cad625d4019668c6ab0607e13eb2d7baabe2
Jaime <jaimequermarijuan@gmail.com>
2023-11-05 19:33:10 +0100

    formated code

--- excel/src/excel/index.java
+++ excel/src/excel/index.java
@@ -14,13 +14,12 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class index {
-    // variables que usaremos despues, son static para poder usarlas en otros metodos del index
+    // variables que usaremos despues, son static para poder usarlas en otros
+    // metodos del index
     static Persona persona = new Persona();
     static double total = 0.0;
     static String perNombre = "";
 
-    
-     
     public static void main(String[] args) throws IOException {
         // creamos una lista vacia de tipo listaPersonas
         listaPersonas list = new listaPersonas();
@@ -40,36 +39,41 @@ public class index {
                 int option = sc.nextInt();
                 switch (option) {
                     case 1:
-                    // Ejecuta este codigo en caso de que el usuario inserte 1
+                        // Ejecuta este codigo en caso de que el usuario inserte 1
                         mostrarExcel();
                         break;
 
                     case 2:
-                    // Ejecuta este codigo en caso de que el usuario inserte 2
-                    // creamos un arraylist vacio ya que lo necesitamos poara el objeto listaPersonas
+                        // Ejecuta este codigo en caso de que el usuario inserte 2
+                        // creamos un arraylist vacio ya que lo necesitamos poara el objeto
+                        // listaPersonas
                         ArrayList<String> setListaArrayListStrings = new ArrayList<String>();
                         System.out.println("----------------------------");
                         System.out.println("Inserte su nombre: ");
                         // recogemos el nombre de la persona
                         String perNombre = sc.next();
                         persona.setNombre("Nombre : " + perNombre);
-                        // probamos si el nombre introducido es correcto parseandolo a integer si esto da error todo bien si no ejecuta lo del try
+                        // probamos si el nombre introducido es correcto parseandolo a integer si esto
+                        // da error todo bien si no ejecuta lo del try
                         try {
                             Integer.parseInt(perNombre);
                             System.out.println("Nombre no valido");
                         } catch (Exception e) {
-                            System.out.println("Hola " + perNombre + "\n¿Cual es su DNI?, insertar solo los primeros 7 numeros ");
+                            System.out.println(
+                                    "Hola " + perNombre + "\n¿Cual es su DNI?, insertar solo los primeros 7 numeros ");
                             // el usuario inserta el dni
                             String dniPer = sc.next();
-                            // comprueba la longitud de los numeros del dni y en caso de que NO sea 7 ejecuta lo de dentro en cualquier otro caso continua
-                            if ( dniPer.length() != 8) {
+                            // comprueba la longitud de los numeros del dni y en caso de que NO sea 7
+                            // ejecuta lo de dentro en cualquier otro caso continua
+                            if (dniPer.length() != 8) {
                                 System.out.println("No es un DNI valido");
                                 System.out.println("-----------------------");
                                 dniPer = "No es un DNI valido";
                                 break;
                             }
                             try {
-                                // prueba a pasar a numero los numeros introducidos en caso correcto continua en caso de error ejecuta el catch de abajo, el ultimo
+                                // prueba a pasar a numero los numeros introducidos en caso correcto continua en
+                                // caso de error ejecuta el catch de abajo, el ultimo
                                 Integer.parseInt(dniPer);
                                 System.out.println("¿Cual es la letra de su DNI?");
                                 String ltrDni = sc.next();
@@ -104,15 +108,21 @@ public class index {
                                                 System.out.println("Que producto quiere:\n");
                                                 // aqui pide el id del producto para comprar
                                                 int userImput = sc.nextInt();
-                                                // añadimos datos al arralist que hicimos antes con la funcion recogerdatos, que acepta el userimput y ya saca por pantalla lo que elija el usuario
+                                                // añadimos datos al arralist que hicimos antes con la funcion
+                                                // recogerdatos, que acepta el userimput y ya saca por pantalla lo que
+                                                // elija el usuario
                                                 setListaArrayListStrings.addAll(recogerDatosExcel(userImput));
-                                                // seteamos el arraylist de persona con el resultado de la ejecucion anteriror
+                                                // seteamos el arraylist de persona con el resultado de la ejecucion
+                                                // anteriror
                                                 list.setListaPersonas(setListaArrayListStrings);
                                                 break;
 
                                             case 2:
-                                            // en este caso salimos de la ejecucion, mostrando el nombre, y la lista de la compra de la persona, en orden nombreProducto, idProducto y precio
-                                                System.out.println("La lista de la compra de " + perNombre + "\ncon DNI: " + dniPer + " es:");
+                                                // en este caso salimos de la ejecucion, mostrando el nombre, y la lista
+                                                // de la compra de la persona, en orden nombreProducto, idProducto y
+                                                // precio
+                                                System.out.println("La lista de la compra de " + perNombre
+                                                        + "\ncon DNI: " + dniPer + " es:");
                                                 for (String listaPer : list.getListaPersonas()) {
                                                     System.out.println(listaPer);
                                                 }
@@ -135,7 +145,7 @@ public class index {
                         break;
 
                     case 3:
-                    // Ejecuta este codigo en caso de que el usuario inserte 3
+                        // Ejecuta este codigo en caso de que el usuario inserte 3
                         System.out.println("Saliendo del programa");
                         // Cierra la condicion inicial del bucle y coratara el bucle inicial while
                         initCondition = false;
@@ -157,7 +167,8 @@ public class index {
         XSSFRow row;
         FileInputStream fis;
         try {
-            // Abre un flujo de entrada para el archivo "Productos.xlsx" ubicado en la carpeta "excel"
+            // Abre un flujo de entrada para el archivo "Productos.xlsx" ubicado en la
+            // carpeta "excel"
             fis = new FileInputStream(new File("excel\\Productos.xlsx"));
             // Crea un objeto XSSFWorkbook para trabajar con el archivo Excel
             XSSFWorkbook workbook = new XSSFWorkbook(fis);
@@ -191,7 +202,8 @@ public class index {
                                 }
                                 break;
                             default:
-                                // En caso de que el tipo de dato no sea numérico ni cadena, imprime un mensaje de error
+                                // En caso de que el tipo de dato no sea numérico ni cadena, imprime un mensaje
+                                // de error
                                 System.out.println("Error, tipo de dato no soportado: " + cell.getColumnIndex());
                         }
                     }
@@ -206,86 +218,81 @@ public class index {
             e.printStackTrace();
         }
     }
-    
 
-    
-     
-    
- 
-public static ArrayList<String> recogerDatosExcel(int userInput) {
-    // Lista para almacenar la información del producto seleccionado.
-    ArrayList<String> productInfo = new ArrayList<>();
+    public static ArrayList<String> recogerDatosExcel(int userInput) {
+        // Lista para almacenar la información del producto seleccionado.
+        ArrayList<String> productInfo = new ArrayList<>();
 
-    XSSFRow row;
-    FileInputStream fis;
-    String productNameSS = "";
-    String IdProductoDevolverS = "";
-    String productPriceS = "";
-    double returnPrice = 0.0;
+        XSSFRow row;
+        FileInputStream fis;
+        String productNameSS = "";
+        String IdProductoDevolverS = "";
+        String productPriceS = "";
+        double returnPrice = 0.0;
 
-    try {
-        // Abre el archivo Excel para lectura.
-        fis = new FileInputStream(new File("excel\\Productos.xlsx"));
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
-        XSSFSheet spreadsheet = workbook.getSheetAt(0);
-        Iterator<Row> rowIterator = spreadsheet.iterator();
+        try {
+            // Abre el archivo Excel para lectura.
+            fis = new FileInputStream(new File("excel\\Productos.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet spreadsheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = spreadsheet.iterator();
 
-        boolean firstRow = true;
+            boolean firstRow = true;
 
-        while (rowIterator.hasNext()) {
-            row = (XSSFRow) rowIterator.next();
+            while (rowIterator.hasNext()) {
+                row = (XSSFRow) rowIterator.next();
 
-            // Ignora la primera fila (encabezados).
-            if (firstRow) {
-                firstRow = false;
-                continue;
-            }
+                // Ignora la primera fila (encabezados).
+                if (firstRow) {
+                    firstRow = false;
+                    continue;
+                }
 
-            Iterator<Cell> cellIterator = row.cellIterator();
+                Iterator<Cell> cellIterator = row.cellIterator();
 
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                int cellCol = cell.getColumnIndex();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    int cellCol = cell.getColumnIndex();
 
-                if (cellCol == 1) {
-                    int productID = (int) cell.getNumericCellValue();
-                    Integer g = ((Integer) productID);
-                    String IdProductoDevolver = g.toString();
+                    if (cellCol == 1) {
+                        int productID = (int) cell.getNumericCellValue();
+                        Integer g = ((Integer) productID);
+                        String IdProductoDevolver = g.toString();
 
-                    // Si el ID del producto coincide con la entrada del usuario.
-                    if (productID == userInput) {
-                        // Obtiene el nombre del producto y su precio.
-                        String productName = row.getCell(0).getStringCellValue();
-                        double productPrice = row.getCell(3).getNumericCellValue();
-                        total = productPrice + total;
+                        // Si el ID del producto coincide con la entrada del usuario.
+                        if (productID == userInput) {
+                            // Obtiene el nombre del producto y su precio.
+                            String productName = row.getCell(0).getStringCellValue();
+                            double productPrice = row.getCell(3).getNumericCellValue();
+                            total = productPrice + total;
 
-                        // Agrega la información del producto a la lista.
-                        productInfo.add(productName);
-                        productNameSS = productName;
-                        productInfo.add(IdProductoDevolver);
-                        IdProductoDevolverS = IdProductoDevolver;
-                        productInfo.add(String.valueOf(productPrice));
-                        int a = (int) productPrice;
-                        returnPrice = (double) a;
+                            // Agrega la información del producto a la lista.
+                            productInfo.add(productName);
+                            productNameSS = productName;
+                            productInfo.add(IdProductoDevolver);
+                            IdProductoDevolverS = IdProductoDevolver;
+                            productInfo.add(String.valueOf(productPrice));
+                            int a = (int) productPrice;
+                            returnPrice = (double) a;
+                        }
                     }
                 }
             }
+
+            // Imprime información relevante.
+            System.out.println(productNameSS);
+            System.out.println(returnPrice);
+            System.out.println("Total : " + total);
+            System.out.println("-------------------");
+
+            // Cierra el archivo Excel.
+            workbook.close();
+
+            return productInfo;
+        } catch (IOException e) {
+            System.out.println("Error al acceder al archivo Excel");
+            return productInfo;
         }
-
-        // Imprime información relevante.
-        System.out.println(productNameSS);
-        System.out.println(returnPrice);
-        System.out.println("Total : " + total);
-        System.out.println("-------------------");
-
-        // Cierra el archivo Excel.
-        workbook.close();
-
-        return productInfo;
-    } catch (IOException e) {
-        System.out.println("Error al acceder al archivo Excel");
-        return productInfo;
     }
-}
 
 }
